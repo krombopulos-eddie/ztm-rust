93f69329a62323a84978771806d9488b449bdfaf
Eddie rios <krombopulos.eddie@gmail.com>
2023-05-06 21:29:35 -0700

    a1 complete

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -5,7 +5,18 @@
 //
 // Notes:
 // * Use a function to display your first name
+fn first_name() {
+    println!("Eddie");
+}
 // * Use a function to display your last name
+fn last_name() {
+    println!("Rios");
+}
+
+
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    first_name();
+    last_name();
+}
